f88b17ed59b2272e6e27facb5062d3a17f072167
Fazila Nugraha Dhiyaa <nugrahafazila@gmail.com>
2026-05-27 20:44:13 +0700

    UPDATE:title & desc updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "pajil",
-  description: "portofolio",
+  title: "Fazil's Portofolio & Production",
+  description: "Welcome to Fazil's Portofolio & Production, where creativity meets innovation. Explore a curated collection of projects, showcasing expertise in design, development, and production. From captivating visuals to cutting-edge solutions, discover the essence of Fazil's work and passion for delivering exceptional results. Join us on this journey of creativity and excellence.",
 };
 
 export default function RootLayout({
